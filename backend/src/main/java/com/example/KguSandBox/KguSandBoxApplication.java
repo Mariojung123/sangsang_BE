e4425164212f81d195a39294aa3d0cac0402c525
Mariojung123 <mario@kyonggi.ac.kr>
2024-06-06 15:51:53 +0900

    도커 이전 스프링 기초 세팅

---
+++ backend/src/main/java/com/example/KguSandBox/KguSandBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.KguSandBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KguSandBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KguSandBoxApplication.class, args);
+	}
+
+}
