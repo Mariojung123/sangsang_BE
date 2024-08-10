8f3427cc5ddcfd87f584f5e293c66418e2f13d9c
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-08-10 18:04:34 +0900

    php 문제 수정

--- backend/src/main/resources/static/problem/sqli/php/sqli.php
+++ backend/src/main/resources/static/problem/sqli/php/sqli.php
@@ -4,27 +4,4 @@ if (isset($_POST['ip'])) {
 
     # append code here
 }
-?>`,
-                'python' :
-`from flask import Flask, render_template, request
-import os, subprocess
-
-app = Flask(__name__)
-app.secret_key = os.urandom(32)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/encode', methods=['POST'])
-def encode():
-    # append encode code
-    pass
-
-@app.route('/decode', methods=['POST'])
-def decode():
-    # append decode code
-    pass
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+?>
